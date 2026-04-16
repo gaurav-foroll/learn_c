85d3b9c12aac03e0ae81841e548ff86a46ecdaa5
gaurav-foroll <gm890205@gmail.com>
2026-04-16 08:26:47 +0530

    done till step 77 vertical and horizontal page scroll/shift works now

--- editor/kilo.c
+++ editor/kilo.c
@@ -51,6 +51,7 @@ struct editorConfig {
     
     int cx, cy; 
     int rowoff; // this actually gives us the screen offset value for actual row user is on we take filerow below
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -284,6 +285,12 @@ void editorScroll(void) {
 
         E.rowoff = E.cy - E.screenrows + 1;
     }
+    if(E.cx < E.coloff){
+        E.coloff = E.cx;
+    }
+    if(E.cx >= E.coloff + E.screencols){
+        E.coloff = E.cx - E.screencols + 1;
+    }
 }
 
 void editorDrawRows(struct abuf* ab) {
@@ -313,9 +320,10 @@ void editorDrawRows(struct abuf* ab) {
 
         } else {
 
-            int len = E.row[filerow].size;
+            int len = E.row[filerow].size - E.coloff;
+            if(len < 0) len = 0;
             if(len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row[filerow].chars, len);
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len); // handling linewise the amount of text to be displayed
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -339,7 +347,7 @@ void editorRefreshScreen(void) {
     editorDrawRows(&ab); // draw ~ in each row
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1); // basically translationg file position to position on  terminal
     abAppend(&ab, buf, strlen(buf));
     abAppend(&ab, "\x1b[?25h", 6);
     write(STDIN_FILENO, ab.b, ab.len);
@@ -354,6 +362,8 @@ void editorRefreshScreen(void) {
 
 void editorMoveCursor(int key) {
 
+    erow* row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch(key) {
 
         case ARROW_LEFT:
@@ -362,7 +372,7 @@ void editorMoveCursor(int key) {
             }
             break;
         case ARROW_RIGHT:
-            if (E.cx != E.screencols - 1) {
+            if (row && E.cx < row->size) {
                 E.cx++;
             }
             break;
@@ -377,6 +387,12 @@ void editorMoveCursor(int key) {
             }
             break;
     }
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen  = row ? row->size : 0;
+    if(E.cx > rowlen) {
+        E.cx = rowlen;
+    } 
 
 
 }
@@ -431,6 +447,7 @@ void initEditor(void) {
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
     if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
