4883b0175f6349aa92558003db17cbce2605801c
gaurav-foroll <gm890205@gmail.com>
2026-04-18 19:42:18 +0530

    working on handling tabs in the editor created a new render and rsize in erow to show the rendered row and replacing size and chars so far till step 83

--- editor/kilo.c
+++ editor/kilo.c
@@ -43,7 +43,10 @@ enum editorKey {
 typedef struct erow {
 
     int size;
+    int rsize;
     char *chars;
+    char* render;
+
 
 } erow; // struct for editor row
 
@@ -209,6 +212,22 @@ int getWindowSize(int* rows, int* cols) {
 
 /*** row operations ***/
 
+void editorUpdateRow(erow* row) {
+
+    free(row->render);
+    row->render = malloc(row.size + 1);
+    int j;
+    int idx = 0;
+    for ( j = 0; j < row->size; j++ ) {
+        row->render[idx++] = row->chars[j];
+
+    }
+    row->render[idx] = '\0';
+    row->rsize = idx;
+
+
+}
+
 void editorAppendRow(char* s, size_t len) {
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
     int at = E.numrows;
@@ -216,6 +235,10 @@ void editorAppendRow(char* s, size_t len) {
     E.row[at].chars = malloc(len + 1); 
     memcpy(E.row[at].chars, s, len);
     E.row[at].chars[len] = '\0';
+
+    E.row[at].rsize = 0;
+    E.row[at].render = NULL;
+    editorUpdateRow(&E.row[at]);
     E.numrows++;
 }
 
@@ -320,10 +343,10 @@ void editorDrawRows(struct abuf* ab) {
 
         } else {
 
-            int len = E.row[filerow].size - E.coloff;
+            int len = E.row[filerow].rsize - E.coloff;
             if(len < 0) len = 0;
             if(len > E.screencols) len = E.screencols;
-            abAppend(ab, &E.row[filerow].chars[E.coloff], len); // handling linewise the amount of text to be displayed
+            abAppend(ab, &E.row[filerow].render[E.coloff], len); // handling linewise the amount of text to be displayed
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -369,11 +392,17 @@ void editorMoveCursor(int key) {
         case ARROW_LEFT:
             if (E.cx != 0) {
                 E.cx--;
+            } else if (E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
             }
             break;
         case ARROW_RIGHT:
             if (row && E.cx < row->size) {
                 E.cx++;
+            } else if (row && E.cx == row->size) {
+                E.cy++;
+                E.cx = 0;
             }
             break;
         case ARROW_UP:
